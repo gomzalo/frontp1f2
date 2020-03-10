9527d790438ca38898b04d314794444d2ae6149a
g <2205280200101@ingenieria.usac.edu.gt>
2020-03-10 03:06:59 -0600

    Index working?

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-app.listen('3000', function() {
-  console.log('Servidor web escuchando en el puerto 3000');
-});
+// app.listen('3000', function() {
+//   console.log('Servidor web escuchando en el puerto 3000');
+// });
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+var bodyParser = require('body-parser');
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get('/', function(req, res) {
+        res.sendFile('public/index.html', {root: __dirname });
+    })
+
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
