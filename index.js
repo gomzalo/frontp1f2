dae8a0bcb03b18213e34f1fcc43f44f01e9e3b63
g <2205280200101@ingenieria.usac.edu.gt>
2020-03-10 03:00:43 -0600

    INdex

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen('3000', function() {
   console.log('Servidor web escuchando en el puerto 3000');
